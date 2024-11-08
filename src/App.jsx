3db3c90bdb84df078b31420c35c6c744bc61bf6d
wacho91 <criskol.71@gmail.com>
2024-11-08 10:08:43 -0500

    Navbar Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <main className="overflow-x-hidden">
       Hello world!
-    </h1>
+    </main>
   )
 }
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+  return (
+    <div>
+      Navbar Component
+    </div>
+  )
+}
+
+export default Navbar
