33760a8ffa31bf0f0f557ee7c586d80fddf7d1e6
wacho91 <criskol.71@gmail.com>
2024-11-12 11:05:41 -0500

    Navbar Complete

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,9 +1,43 @@
+import { MdMenu } from "react-icons/md";
+import {NavbarData} from "../data/MockData";
+import { FaApple, FaRegUser } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar Component
-    </div>
+    <nav className="text-white py-5">
+      <div className="container flex justify-between items-center">
+          <div className="flex items-center gap-2 text-3xl font-semibold">
+            <FaApple />
+            AirPods Max 
+          </div>
+
+          <div className="hidden md:block">
+            <ul className="flex items-end gap-4"> 
+              {
+                NavbarData.map((item) => {
+                  return (
+                    <li key={item.id}>
+                      <a 
+                        href={item.link}
+                        className="inline-block text-base py-2 px-3 uppercase"
+                      >
+                        {item.title}
+                      </a>
+                    </li>
+                  )
+                })
+              }
+              <button className="text-xl ps-14">
+                <FaRegUser/>
+              </button>
+            </ul>
+          </div>
+
+          <div className="hidden">
+            <MdMenu  className="text-4xl"/>
+          </div>
+      </div>
+    </nav>
   )
 }
 
